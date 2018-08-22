435b811f637d2871b728232b627fc9de258f0234
Sanket Ray <iamsanketray123@gmail.com>
2018-08-22 14:06:46 +0530

    changed to 2

--- Conflicts/ViewController.swift
+++ Conflicts/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let a = 3
+        let a = 2
         
         print("Hello Sanket")
     }
